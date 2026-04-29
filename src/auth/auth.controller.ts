3a87a191386f032cba241385053a5533cf327bb7
Mohammed Tanvir <tanvir.eatl@gmail.com>
2026-04-30 04:51:37 +0600

    Fix: status issue fixed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { Public } from 'src/decorators/public.decorator';
 
-@Controller('auth')
+@Controller('')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Public()
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   async login(@Body() dto: LoginDto) {
     const { sessionToken, user } = await this.authService.createUser(dto);
 
--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -73,7 +73,7 @@ export class RoomsController {
 
     return {
       success: true,
-      message: 'Room deleted successfully',
+      data: { deleted: true },
     };
   }
 }
